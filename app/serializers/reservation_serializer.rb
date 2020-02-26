792e9ad85a22be5d5ffb10828f3edeae354da49f
RadenAR <radenabdulrahman@gmail.com>
2020-02-26 09:20:29 -0500

    Added editable functionality for reservations

--- app/serializers/reservation_serializer.rb
+++ app/serializers/reservation_serializer.rb
@@ -1,5 +1,9 @@
 class ReservationSerializer < ActiveModel::Serializer
-  attributes :id, :people, :date, :time
+  attributes :id, :people, :date, :time, :editable
   has_one :restaurant
   has_one :user
+
+  def editable
+    scope == object.user
+  end
 end
